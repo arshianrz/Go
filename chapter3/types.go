901b12fbe4c5867c4da2658b469394f6e519a6b8
arshianrz <arshia.nozarnezhad@gmail.com>
2020-09-23 12:18:23 +0330

    Sep 23

---
+++ chapter3/types.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1+1 =", 1+1)            // Sum of 2 integers.
+	fmt.Println('H')                     // Prints ascii code of H.
+	fmt.Println(len("hey there golang")) // Prints length of the string.
+	fmt.Println("str1" + "str2")         // Connects two strings together. *(not addition)*
+	fmt.Println("Hello World"[1])        //	The second character of the string Hello World ascii code.
+	//Boolean logical operators :
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(false || false)
+	fmt.Println(true && false)
+	fmt.Println(true && true)
+	fmt.Println(false && false)
+	fmt.Println(!false)
+}
