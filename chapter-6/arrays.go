b0545932e78be59e6f7562d5aa703aa47c02f623
arshianrz <arshia.nozarnezhad@gmail.com>
2020-09-28 01:06:48 +0330

    Sep 28

--- chapter-6/arrays.go
+++ chapter-6/arrays.go
@@ -3,30 +3,31 @@ package main
 import "fmt"
 
 func arrays() {
-	var index [5]int
+	var example [5]int
 	index2 := [5]int{1, 2, 3, 4, 5}
 	//2 ways of defining arrays
 	for i := 0; i < 5; i++ {
-		index[i] = 6
+		example[i] = 6
 	}
-	fmt.Println(index)
+	fmt.Println(example)
 	fmt.Println(index2)
 
 }
 
 func arrayInput() {
-	var index [5]int
+	var example [5]int
 	var total float64 = 0
 	for i := 0; i < 5; i++ {
-		fmt.Println("Enter", i, "'s index :")
-		fmt.Scanf("%d", &index[i])
-		total += float64(index[i])
+		fmt.Println("Enter", i, "'s example :")
+		fmt.Scanf("%d", &example[i])
+		total += float64(example[i])
 	}
-	fmt.Println("Your array is", index)
-	fmt.Println("The average of array is :", (total / float64(len(index))))
+	fmt.Println("Your array is", example)
+	fmt.Println("The average of array is :", (total / float64(len(example))))
 
-	for _, value := range index {
+	total = 0
+	for _, value := range example {
 		total += float64(value)
 	}
-	fmt.Println(total / float64(len(index)))
+	fmt.Println(total / float64(len(example)))
 }
