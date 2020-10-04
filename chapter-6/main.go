7fa12db21090a8115094f3a911125c2cd306808c
arshianrz <arshia.nozarnezhad@gmail.com>
2020-10-04 18:37:39 +0330

    added slice examples

--- chapter-6/main.go
+++ chapter-6/main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	//arrays()
-	arrayInput()
+	//arrayInput()
+	slices()
 }
--- chapter-6/slices.go
+++ chapter-6/slices.go
@@ -1,5 +1,21 @@
 package main
 
-func slices() {
+import "fmt"
 
+func slices() {
+	var input int
+	var temp float64
+	var example []float64
+	fmt.Println("How large is your array?")
+	fmt.Scanf("%d", &input)
+	for i := 0; i <= input; i++ {
+		fmt.Println("Enter", i, "'s element.")
+		fmt.Scanf("%f", &temp)
+		/*example = make([]float64, input)
+		if i == 0 {
+			fmt.Println(example)
+		}*/
+		example = append(example, temp)
+	}
+	fmt.Println(example)
 }
